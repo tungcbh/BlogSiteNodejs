b3b95111e89b2474b347ceaec5823487626d2e4b
nttung42 <ntt.a5k62.cbh@gmail.com>
2024-03-10 22:01:02 +0700

    Day la ban cu

---
+++ server.js
@@ -0,0 +1,57 @@
+// const http = require('http');
+// const fs = require('fs');
+// const _ = require('lodash');
+
+
+// const server = http.createServer((req, res) => {
+//     console.log(req.url, req.method);
+//     // lodash
+//     const num = _.random(10, 1000);
+//     console.log(num);
+
+//     // set header content type
+//     res.setHeader('Content-Type', 'text/html');
+
+//     let path = './views/';
+//     switch(req.url) {
+//         case '/':
+//             path += 'index.html';
+//             res.statusCode = 200;
+//             break;
+//         case '/about':
+//             path += 'about.html';
+//             res.statusCode = 200;
+//             break;
+//         case '/about-me':
+//             res.statusCode = 301;
+//             res.setHeader('Location', '/about');
+//             res.end();
+//         default: 
+//             path += '404.html';
+//             res.statusCode = 404;
+//             break;
+//     }
+
+
+//     fs.readFile(path, (err, data) => {
+//         if (err) {
+//             console.log(err);
+//             res.end();
+//         }
+//         else {
+//             res.write(data);
+//             res.end();
+//         }
+//     });
+
+
+//     // res.write('<head><link rel = "stylesheet" href = "#"></head>')
+//     // res.write('<p>Hello, ninjas</p>');
+//     // res.write('<p>Hello again??</p>');
+//     // res.end();
+
+// });
+
+// server.listen(3000, 'localhost', () => {
+//     console.log('listening for request on port 3000');
+// })
